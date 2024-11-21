eb4f073064ba87c7f38333b5c3840c6869fada7e
IFTE-13 <mariamnoor1290@gmail.com>
2024-11-21 22:15:18 +0600

    Fixed: lint issues#3

--- components/services/Card.tsx
+++ components/services/Card.tsx
@@ -71,8 +71,6 @@ export function Card() {
               <CardItem
                 translateZ={20}
                 as={Link}
-                href="https://twitter.com"
-                target="__blank"
                 className="px-4 py-2 rounded-xl text-xs font-normal dark:text-white"
               >
                 Book now →
